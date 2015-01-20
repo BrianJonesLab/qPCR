cf60eb245425dc1c659b9731d6ab8c87f3b8028e
BrianJonesLab <brianjoneslab@gmail.com>
2015-01-20 11:51:49 +1100

    the functions and a sata example

---
+++ qPCRV4fUNCTIONS.R
@@ -0,0 +1,130 @@
+############ FUNCTION 1 ############################################
+# Parameters function, determine all the parameters for the analysis
+
+Par <- list(Start = NULL, End = NULL, dif = NULL, curves = NULL, Eff_min = NULL, Eff_max = NULL, slope_min = NULL, slope_max = NULL)
+Values <- list(it = NULL, Cycles = NULL)
+
+Parameters <- function(Start = 1, End = length(rowdata), dif = 4, rowdata=NULL, Eff_min = 0.6, Eff_max = 1.2){
+    Start  <- Start
+    End <- End
+    dif    <- dif # min number of points used in the curve
+    curves <- nrow(rowdata)
+    Eff_min <- Eff_min
+    Eff_max <- Eff_max
+    slope_min <- 1/log10(Eff_min+1)
+    slope_max <- 1/log10(Eff_max+1)
+    Par <<- list(Start = Start, End = End, dif = dif, curves = curves, 
+                 Eff_min = Eff_min, Eff_max = Eff_max, slope_min = slope_min, slope_max = slope_max)
+    Values <<- list(it = ((End-dif+Start)*(End-dif))/2, Cycles = Start:End)
+}
+
+#####################################################################
+
+############ FUNCTION 2 ############################################
+## We calculate the derivative of all the curves, so we need a matrix length x-1 with same rows as curves
+
+
+## maybe an apply function is best here, but...
+Inflection_point <- function(rawData = NULL){
+    dydx <<- matrix(nrow = Par$curves, ncol = Par$End - 1)
+    for(curv in 1:Par$curves){
+        for(i in 1:(Par$End - 1)){
+            dydx[curv,i] <<- (rawData[curv,(i + 1)]-rawData[curv,i])
+        }
+    }
+}
+
+#####################################################################
+
+############ New Object 1 ###########################################
+inf_curves <- data.frame(Well = NA, inf_point = NA, a = NA, b = NA, "R^2" = NA, 
+                         "slope(x~y)" = NA, CtY0 = NA, Eff = NA, "Eff'%'" = NA, Ri = NA)         
+# the second value is the inlfection point
+
+
+#####################################################################
+
+
+############ FUNCTION 3 ############################################
+## very optimizable function, but this in theory will put in log any data that I have.
+
+log_Data <- function(rawData = NULL,Name = "logData"){
+    namelogdata <- paste(Name,"log_Data",sep = "_")
+    log_Data <- matrix(nrow = Par$curves, ncol = Par$End)
+    for (cur in 1:Par$curves){
+        linetested <- rawData[cur,] # to put in the loop the curve that is tested
+        minval <- min(linetested[linetested > 0])   # to check the min value on each curve 
+        log_Data[cur,] <- rapply(as.list(rawData[cur,]),function(n){  # in raw data for more curves
+            if(n<0){
+                n <- log10(minval)  # if the value is negative, put the log of the min to avoid ERROR
+            } else {
+                n <- log10(n)
+            }
+        })
+    }
+    assign(namelogdata,log_Data,envir = .GlobalEnv)
+}
+
+#####################################################################
+
+
+############ FUNCTION 4 ############################################
+# TEST
+# log_Data = Rod1_log_Data
+# cur = 2
+# rm(log_Data)
+# rm(cur)
+# rm(bestline)
+# rm(count)
+
+
+Init_Value <- function (log_Data = NULL){
+    for (cur in 1:Par$curves){ # first loop to apply all the script to each curve, should be an apply formula but...
+        # matrix to record the R^2 of combinations
+        bestline <- matrix(data = NA, nrow = Values$it, ncol = 5) #should be out of the loop and the nrow in this case is the max iteraction
+        count <- 1 # From here could be possible to add the interaction values in relation to each "cur"
+        
+        #points <- list(1:inf_curves[cur,2],inf_curves[cur,2]:1)
+        #p1_p2 <- expand.grid(points)
+        #p1_p2[1,1]
+        #p1_p2[1,2]    
+        #we can use this for any apply but for the moment I don't know and this works
+        #cur<-1
+        #log_Data <- Neilraw_log_Data
+        #i<-21
+        #j<-25
+        
+        
+        for (i in Par$Start:inf_curves[cur,2]) {        # in this case is inflection point
+            for (j in inf_curves[cur,2]:Par$Start) {
+                if ( (i < j) & (j-i >= Par$dif) ) {
+                    Temp_Data_lm <- lm((i:j) ~ (log_Data[cur,(i:j)])) # known Y ~ known X, to get the right slope
+                    bestline[count,1:2] <- (c(i,j))# 3 a, 4 b
+                    bestline[count,3] <- summary(Temp_Data_lm)$r.squared # 5 R^2
+                    bestline[count,4] <- Temp_Data_lm$coefficients[[2]] # 6 slope(x~y)
+                    bestline[count,5] <- Temp_Data_lm$coefficients[[1]] # 7 CtY0 cycle where R is 0
+                    count <- count + 1
+                } 
+            }
+        }
+        bestline <- bestline[(bestline[,4] < Par$slope_min) & (bestline[,4] > Par$slope_max),] 
+        bestline <- bestline[complete.cases(bestline),]
+        bestline <- as.matrix(bestline)
+        if(ncol(bestline) == 1) bestline <- t(bestline)
+        
+        if(nrow(bestline)!=0){       # To skip the error
+            inf_curves[cur,3:7]  <<- bestline[which.max(bestline[,3]),] #copy the best R^2 line to info curves
+            inf_curves[cur,8]    <<- (10^(1/inf_curves[cur,6])-1)+1 # 8 convert the slope in efficiency
+            inf_curves[cur,9]    <<- (inf_curves[cur,8]-1)*100 # 9 slope in %, probably unecesary
+            inf_curves[cur,10]   <<- 10^(-inf_curves[cur,7]/inf_curves[cur,6]) 
+                                                        # 10 Ri, is the anti log of C when Y is 0
+                                                        # or the level of emmission at the cycle 0
+        } else {
+            inf_curves[cur,3:10] <<- NA
+        }
+    }
+}
+
+#####################################################################
+
+
---
+++ qPCRsampleorderNeilV4.R
@@ -0,0 +1,132 @@
+ source("qPCRcodeV4Neil.R")
+
+# data from Neil
+
+head(inf_curves)
+
+Samples <- read.table("Neilsamples.txt", fill = T)
+# samples could be control or treatment, and will have biological replicates
+# also the initial concentration of some samples could be known, but by default
+# control initial concentration will be 1 and treatment X. T/C
+
+Genes <- read.table("Neilgenes.txt", fill = T, header = T)
+# genes could be reference or target. the reference gene had a value 1 and the target is x
+# And the calculation of the target gene is in base of the reference t/r
+
+
+p1s <- read.table("Neilplate1samples.txt", fill = T, header = T, nrows = 8) 
+p1s[,1] <- NULL
+p1s[,12] <- NA
+colnames(p1s) <- 1:12
+rownames(p1s) <- LETTERS[1:8]
+
+p1g <- read.table("Neilplate1genes.txt", fill = T, header = T, nrows = 8) 
+p1g[,1] <- NULL
+p1g[,12] <- NA
+colnames(p1g) <- 1:12
+rownames(p1g) <- LETTERS[1:8]
+
+p2s <- read.table("Neilplate2samples.txt", fill = T, header = T, nrows = 8) 
+p2s[,1] <- NULL
+p2s[,12] <- NA
+colnames(p2s) <- 1:12
+rownames(p2s) <- LETTERS[1:8]
+
+p2g <- read.table("Neilplate2genes.txt", fill = T, header = T, nrows = 8) 
+p2g[,1] <- NULL
+p2g[,12] <- NA
+colnames(p2g) <- 1:12
+rownames(p2g) <- LETTERS[1:8]
+
+# check technical replicates
+# Put the gene and sample in a new column to check as a factor and search for tech rep
+# put together the tech rep as list to plot and check and average.
+plates <- inf_curves[,1]
+
+tmp <- strsplit(inf_curves$Well,'_')
+tmp <- do.call(rbind,tmp)
+
+paste0('p',tmp[,1],'g')
+tmp[,2]
+
+find_gene <- function(plate,well){
+    plate_ <- get(plate,envir=.GlobalEnv)
+    x <- regmatches(well,regexpr('\\d+',well))
+    y <- regmatches(well,regexpr('[a-zA-Z]',well))
+    as.character(plate_[which(rownames(plate_) == y),as.numeric(x)])
+}
+
+
+inf_curves$gene <- mapply(find_gene,plate=paste0('p',tmp[,1],'g'),well=tmp[,2])
+inf_curves$sample <- mapply(find_gene,plate=paste0('p',tmp[,1],'s'),well=tmp[,2])
+inf_curves$code <- paste(inf_curves$gene,inf_curves$sample,sep = "_") # we culd use a code.. match(inf_curves$gene[1],Genes[,1])
+    
+head(inf_curves)
+
+#technical replicates
+
+tRepAv <- tapply(inf_curves$Ri,inf_curves$code,mean)
+tRepall <- tapply(inf_curves$Ri,inf_curves$code,function(x) x)
+tRepDIF <- tapply(inf_curves$Ri,inf_curves$code,function(x) max(x)-min(x))
+
+ref <- as.character(Genes[Genes$Condition == 'Reference',1])
+
+comb <- unique(regmatches(names(tRepAv),regexpr('\\d+[_]\\d+',names(tRepAv))))
+normalised <- sapply(comb,function(x){
+    sapply(USE.NAMES=F,names(tRepAv)[grepl(x,names(tRepAv)) & !grepl(ref,names(tRepAv))],function(y){
+        tRepAv[y]/tRepAv[paste0(ref,'_',x)]
+    })  
+})
+
+normalised <- unlist(normalised)
+
+
+
+gen <- "SodA"
+biorep_c = '1_1'
+biorep_t = '2_1'
+treatment <- sapply(simplify='array',as.character(Genes[Genes$Condition == 'Target',1]),function(gen){
+    selected_gen <- normalised[grepl(gen,names(normalised))]
+    sapply(comb[grepl(paste0(as.character(which(Samples$Group == 'Control')),'[_]'),comb)],function(biorep_c){
+        sapply(comb[!grepl(paste0(as.character(which(Samples$Group == 'Control')),'[_]'),comb)],function(biorep_t){
+            normalised[grepl(paste0(gen,'_',biorep_t),names(normalised))]/normalised[grepl(paste0(gen,'_',biorep_c),names(normalised))]
+        })
+    })
+})
+
+results_mean <- apply(treatment,3,function(gen){
+    sapply(unique(gsub('(?<=[_]).+','',rownames(gen),perl = T)),function(t){
+        mean(unlist(gen[grepl(t,rownames(gen)),]),na.rm = T)
+    })
+})
+
+results_mean <- reshape2::melt(results_mean)
+
+results_sd <- apply(treatment,3,function(gen){
+    sapply(unique(gsub('(?<=[_]).+','',rownames(gen),perl = T)),function(t){
+        sd(unlist(gen[grepl(t,rownames(gen)),]),na.rm = T)
+    })
+})
+
+results_sd <- reshape2::melt(results_sd)
+to_plot <- cbind(results_mean,results_sd$value)
+colnames(to_plot) <- c('Sample','Gene','mean','sd')
+
+library(ggplot2)
+ggplot(to_plot,aes(x=Gene,y=log2(mean))) + geom_bar(aes(fill = Sample), position = "dodge", stat="identity") +
+    geom_errorbar(aes(ymin=log2(mean-sd), ymax=log2(mean+sd),group=Sample),width=.1,position=position_dodge(.9))
+
+
+summary(as.factor(inf_curves$code))
+
+# cy for tech rep can be CtY0
+
+
+#Samples 3G, 3N, 3B
+
+
+# Plate
+# Sample 1 (3G) Control
+# Sample 2,3 (3N,3B) Treatment
+# RefGene ParA
+# TarGene SodA, OtsA, Rpon2, aceA, LtpD, pspA
